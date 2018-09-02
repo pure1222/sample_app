8f9d2f96c40b86091c4b5cc4c613b68e33933ece
pure1222 <msyaaa634@yahoo.co.jp>
2018-09-02 13:20:29 +0000

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '~> 5.2.1'
 
 gem 'bootstrap-sass', '3.3.7'
 
+gem 'bcrypt',         '3.1.12'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,6 +19,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
+
   test "should get contact" do
     get contact_path
     assert_response :success
